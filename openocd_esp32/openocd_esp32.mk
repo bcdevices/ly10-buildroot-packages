78e1068781164848af5875cc573c822ed68f5db2
Eric Rauer <erauer@users.noreply.github.com>
2022-05-16 15:27:02 -0700

    PLTOS-41 - Remove openssh_ly, openocd_ly, update _zephyr, _esp32, and _cypress openocd (#14)

--- openocd_esp32/openocd_esp32.mk
+++ openocd_esp32/openocd_esp32.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENOCD_ESP32_VERSION = v0.10.0-esp32-20201202
+OPENOCD_ESP32_VERSION = v0.11.0-esp32-20220411 
 OPENOCD_ESP32_SOURCE = v$(OPENOCD_ESP32_VERSION).tar.gz
 OPENOCD_ESP32_SITE = git://github.com/espressif/openocd-esp32
 OPENOCD_ESP32_GIT_SUBMODULES = YES
@@ -14,15 +14,15 @@ OPENOCD_ESP32_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -std=gnu99"
 OPENOCD_ESP32_DEPENDENCIES = libusb-compat
 OPENOCD_ESP32_DEPENDENCIES += libftdi
 OPENOCD_ESP32_DEPENDENCIES += hidapi
+OPENOCD_ESP32_DEPENDENCIES += jimtcl
 
 OPENOCD_ESP32_CONF_OPTS += --prefix=/usr/local/esp/openocd
 OPENOCD_ESP32_CONF_OPTS += --enable-ftdi
 OPENOCD_ESP32_CONF_OPTS += --disable-doxygen-html
 OPENOCD_ESP32_CONF_OPTS += --includedir=$(STAGING_DIR)/usr/include/libusb-1.0
-OPENOCD_ESP32_CONF_OPTS += --with-jim-shared=no
-OPENOCD_ESP32_CONF_OPTS += --disable-shared
 OPENOCD_ESP32_CONF_OPTS += --enable-dummy
 OPENOCD_ESP32_CONF_OPTS += --disable-werror
+OPENOCD_ESP32_CONF_OPTS += --disable-shared
 OPENOCD_ESP32_CONF_OPTS += --disable-internal-jimtcl 
 
 define OPENOCD_ESP32_BOOTSTRAP
@@ -31,7 +31,7 @@ define OPENOCD_ESP32_BOOTSTRAP
 endef
 
 define OPENOCD_ESP32_TARGET_RENAME
-	mv $(TARGET_DIR)/usr/bin/openocd $(TARGET_DIR)/usr/bin/openocd_zephyr
+	mv $(TARGET_DIR)/usr/bin/openocd $(TARGET_DIR)/usr/bin/openocd_esp32
 endef
 
 
--- openocd_zephyr/openocd_zephyr.mk
+++ openocd_zephyr/openocd_zephyr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENOCD_ZEPHYR_VERSION = a0e8edc4e70b2bc35de57f44717a289d9e651f6f
+OPENOCD_ZEPHYR_VERSION = v0.11.0 
 OPENOCD_ZEPHYR_SITE = git://github.com/zephyrproject-rtos/openocd.git
 OPENOCD_ZEPHYR_GIT_SUBMODULES = YES
 
@@ -13,6 +13,7 @@ OPENOCD_ZEPHYR_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -std=gnu99"
 OPENOCD_ZEPHYR_DEPENDENCIES = libusb-compat
 OPENOCD_ZEPHYR_DEPENDENCIES += libftdi
 OPENOCD_ZEPHYR_DEPENDENCIES += hidapi
+OPENOCD_ZEPHYR_DEPENDENCIES += jimtcl
 
 OPENOCD_ZEPHYR_CONF_OPTS += --prefix=/usr/local/zephyr/openocd
 OPENOCD_ZEPHYR_CONF_OPTS += --enable-ftdi
@@ -21,7 +22,6 @@ OPENOCD_ZEPHYR_CONF_OPTS += --enable-cmsis-dap
 OPENOCD_ZEPHYR_CONF_OPTS += --enable-jlink
 OPENOCD_ZEPHYR_CONF_OPTS += --enable-stlink
 OPENOCD_ZEPHYR_CONF_OPTS += --includedir=$(STAGING_DIR)/usr/include/libusb-1.0
-OPENOCD_ZEPHYR_CONF_OPTS += --with-jim-shared=no
 OPENOCD_ZEPHYR_CONF_OPTS += --disable-shared
 OPENOCD_ZEPHYR_CONF_OPTS += --enable-dummy
 OPENOCD_ZEPHYR_CONF_OPTS += --disable-werror
@@ -37,8 +37,8 @@ define OPENOCD_ZEPHYR_TARGET_RENAME
 	mv $(TARGET_DIR)/usr/bin/openocd $(TARGET_DIR)/usr/bin/openocd_zephyr
 endef
 
+OPENOCD_ZEPHYR_PRE_PATCH_HOOKS += OPENOCD_ZEPHYR_BOOTSTRAP 
 
-OPENOCD_ZEPHYR_PRE_PATCH_HOOKS += OPENOCD_ZEPHYR_BOOTSTRAP
 OPENOCD_ZEPHYR_POST_INSTALL_TARGET_HOOKS += OPENOCD_ZEPHYR_TARGET_RENAME
 
 $(eval $(autotools-package))
