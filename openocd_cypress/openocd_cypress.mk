406245a7cf529f7788c877a5ac746915eec658f5
Eric Rauer <erauer@users.noreply.github.com>
2022-05-17 14:27:06 -0700

    PLTOS-41 - revert openocd cypress and fix build for aarch64 (#16)

--- openocd_cypress/openocd_cypress.mk
+++ openocd_cypress/openocd_cypress.mk
@@ -3,43 +3,48 @@
 # openocd-cypress
 #
 ################################################################################
-
-OPENOCD_CYPRESS_VERSION = release-v4.3.1
-OPENOCD_CYPRESS_SITE = git://github.com/Infineon/openocd.git
+OPENOCD_CYPRESS_VERSION = release-v4.1.0
+OPENOCD_CYPRESS_SITE = git://github.com/cypresssemiconductorco/openocd.git
 OPENOCD_CYPRESS_GIT_SUBMODULES = YES
 
+OPENOCD_CYPRESS_LICENSE = GPL-2.0+
+OPENOCD_CYPRESS_LICENSE_FILES = COPYING
+# 0002-configure-enable-build-on-uclinux.patch patches configure.ac
 OPENOCD_CYPRESS_AUTORECONF = YES
 OPENOCD_CYPRESS_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -std=gnu99"
-OPENOCD_CYPRESS_DEPENDENCIES = libusb-compat
-OPENOCD_CYPRESS_DEPENDENCIES += libftdi
-OPENOCD_CYPRESS_DEPENDENCIES += hidapi
-OPENOCD_CYPRESS_DEPENDENCIES += jimtcl
 
-OPENOCD_CYPRESS_CONF_OPTS += --prefix=/usr/local/cypress/openocd
-OPENOCD_CYPRESS_CONF_OPTS += --enable-ftdi
-OPENOCD_CYPRESS_CONF_OPTS += --disable-doxygen-html
-OPENOCD_CYPRESS_CONF_OPTS += --enable-cmsis-dap
-OPENOCD_CYPRESS_CONF_OPTS += --enable-jlink
-OPENOCD_CYPRESS_CONF_OPTS += --enable-kitprog
-OPENOCD_CYPRESS_CONF_OPTS += --includedir=$(STAGING_DIR)/usr/include/libusb-1.0
-OPENOCD_CYPRESS_CONF_OPTS += --enable-dummy
-OPENOCD_CYPRESS_CONF_OPTS += --disable-werror
-OPENOCD_CYPRESS_CONF_OPTS += --disable-internal-jimtcl 
-OPENOCD_CYPRESS_CONF_OPTS += --disable-shared 
+OPENOCD_CYPRESS_DEPENDENCIES = \
+	host-pkgconf \
+	jimtcl \
+	$(if $(BR2_PACKAGE_LIBFTDI1),libftdi1) \
+	$(if $(BR2_PACKAGE_LIBUSB),libusb) \
+	$(if $(BR2_PACKAGE_LIBUSB_COMPAT),libusb-compat) \
+	$(if $(BR2_PACKAGE_LIBHID),libhid) \
+	$(if $(BR2_PACKAGE_HIDAPI),hidapi) \
+	$(if $(BR2_PACKAGE_LIBGPIOD),libgpiod)
 
+# Adapters
+OPENOCD_CYPRESS_CONF_OPTS = \
+	--oldincludedir=$(STAGING_DIR)/usr/include \
+	--includedir=$(STAGING_DIR)/usr/include \
+	--disable-doxygen-html \
+	--disable-internal-jimtcl \
+	--disable-shared \
+	--enable-dummy \
+	--disable-werror \
+	--prefix=/usr/local/cypress/openocd \
+	--enable-ftdi \
+	--enable-jlink \
+  --enable-kitprog \
+	--enable-cmsis-dap 
 
-OPENOCD_CYPRESS_BIN_ARCH_EXCLUDE = usr/local/cypress/openocd/share/openocd/flm/cypress/traveo2 usr/local/cypress/openocd/share/openocd/flm/cypress/psoc6
-
-define OPENOCD_CYPRESS_BOOTSTRAP
-	rm -rf $(@D)/src/jtag/drivers/libjaylink
-	git -C $(@D)/src/jtag/drivers clone http://repo.or.cz/r/libjaylink.git
-endef
 
 define OPENOCD_CYPRESS_TARGET_RENAME
 	mv $(TARGET_DIR)/usr/bin/openocd $(TARGET_DIR)/usr/bin/openocd_cypress
 endef
 
-OPENOCD_CYPRESS_PRE_PATCH_HOOKS += OPENOCD_CYPRESS_BOOTSTRAP
 OPENOCD_CYPRESS_POST_INSTALL_TARGET_HOOKS += OPENOCD_CYPRESS_TARGET_RENAME
+
+OPENOCD_CYPRESS_BIN_ARCH_EXCLUDE = usr/local/cypress/openocd/share/openocd/flm/cypress/
 
 $(eval $(autotools-package))
