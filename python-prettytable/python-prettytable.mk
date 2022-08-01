a7378a69769cab23873717f8e941bad9dea6eb63
Eric Rauer <github@rauer.dev>
2022-08-01 12:26:25 -0700

    Fix prettytable build

--- python-prettytable/python-prettytable.mk
+++ python-prettytable/python-prettytable.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 PYTHON_PRETTYTABLE_VERSION = 2.5.0
-PYTHON_PRETTYTABLE_SOURCE = $(PYTHON_prettytable_VERSION).tar.gz
-PYTHON_PRETTYTABLE_SITE = git://github.com/jazzband/prettytable
+PYTHON_PRETTYTABLE_SOURCE = prettytable-$(PYTHON_PRETTYTABLE_VERSION).tar.gz
+PYTHON_PRETTYTABLE_SITE = https://files.pythonhosted.org/packages/e4/35/21bf22e21b29102bbe81730caf498dfb3e1eed2642ac71f323472ead673a
 PYTHON_PRETTYTABLE_SETUP_TYPE = setuptools
 PYTHON_PRETTYTABLE_LICENSE = BSD
 
