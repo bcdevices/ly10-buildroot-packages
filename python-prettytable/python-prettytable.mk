33d4ae5a24b1ac98edde20b4fc987fc70281889e
Eric Rauer <github@rauer.dev>
2022-08-01 12:17:55 -0700

    Upgrade prettytable to v2.5.0

--- python-prettytable/python-prettytable.mk
+++ python-prettytable/python-prettytable.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PRETTYTABLE_VERSION = 0.7.2
+PYTHON_PRETTYTABLE_VERSION = 2.5.0
 PYTHON_PRETTYTABLE_SOURCE = $(PYTHON_prettytable_VERSION).tar.gz
 PYTHON_PRETTYTABLE_SITE = git://github.com/jazzband/prettytable
 PYTHON_PRETTYTABLE_SETUP_TYPE = setuptools
