f45abfdf2ef9853fdcb7641829f955f822cbfc8e
Ivo Clarysse <ivo@bcdevices.com>
2023-04-11 08:36:38 +0200

    host-m4

--- plymouth/plymouth.mk
+++ plymouth/plymouth.mk
@@ -11,16 +11,14 @@
 #   graphical boot animation while the boot process happens in the background.
 
 PLYMOUTH_VERSION = 22.02.122
-#PLYMOUTH_STITE = http://www.freedesktop.org/software/plymouth/releases/${BPN}-${PV}.tar.xz
-#SRC_URI[sha256sum] = "100551442221033ce868c447ad6c74d831d209c18ae232b98ae0207e34eadaeb"
 PLYMOUTH_SITE = https://gitlab.freedesktop.org/plymouth/plymouth
 PLYMOUTH_SITE_METHOD = git
-#PLYMOUTH_LICENSE = GPL-2.0+
 PLYMOUTH_LICENSE = GPL-2.0-or-later
 PLYMOUTH_LICENSE_FILES = COPYING
 PLYMOUTH_AUTORECONF = YES
 PLYMOUTH_DEPENDENCIES = host-pkgconf
 PLYMOUTH_DEPENDENCIES += host-gdk-pixbuf
+PLYMOUTH_DEPENDENCIES += host-m4
 
 PLYMOUTH_CONF_OPTS += --enable-drm
 PLYMOUTH_CONF_OPTS += --disable-documentation
