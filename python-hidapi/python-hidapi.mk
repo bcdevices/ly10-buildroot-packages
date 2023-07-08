548ff0e0ecf65971c50ef69b99a6e44ab74352b2
Eric Rauer <eric@rauer.dev>
2023-07-07 19:12:08 -0700

    upgrade python hidapi

--- python-hidapi/python-hidapi.mk
+++ python-hidapi/python-hidapi.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_HIDAPI_VERSION = 0.11.2
+PYTHON_HIDAPI_VERSION = 0.14.0
 PYTHON_HIDAPI_SOURCE = hidapi-$(PYTHON_HIDAPI_VERSION).tar.gz
-PYTHON_HIDAPI_SITE = https://files.pythonhosted.org/packages/c0/36/1ae403d51262337de39bb026b2ccbe588668273c3361f5d49f0e2094f318
+PYTHON_HIDAPI_SITE = https://files.pythonhosted.org/packages/95/0e/c106800c94219ec3e6b483210e91623117bfafcf1decaff3c422e18af349
 PYTHON_HIDAPI_SETUP_TYPE = setuptools
-PYTHON_HIDAPI_LICENSE = FIXME: license id couldn't be detected
+PYTHON_HIDAPI_LICENSE = BSD, GNU General Public License v3 (GPLv3)
 PYTHON_HIDAPI_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
