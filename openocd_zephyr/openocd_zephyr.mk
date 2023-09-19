a54bd47e40fa014e29e963f28ddc4390aea1b6bb
Ivo Clarysse <ivo@bcdevices.com>
2023-09-19 07:29:52 +0200

    openocd_zephyr: bump to sdk-ng v1.16.1 version

--- openocd_zephyr/openocd_zephyr.mk
+++ openocd_zephyr/openocd_zephyr.mk
@@ -4,7 +4,18 @@
 #
 ################################################################################
 
-OPENOCD_ZEPHYR_VERSION = af169e805396e5f7ab926aa32ba293a618a894f7
+# sdk-ng v1.16.1
+OPENOCD_ZEPHYR_VERSION = e2b6f5655e2ccd354fb8c7d0f53b93f4cb42a4f4
+
+# sdk-ng v1.15.1
+#OPENOCD_ZEPHYR_VERSION = c5c47943dbd036079d96fe49ea9092f78abec427
+
+# sdk-ng v1.15.0
+#OPENOCD_ZEPHYR_VERSION = 42b6471c1f6c3236ec762e9a4fde62c84ab6861d
+
+# sdk-ng v1.14.2
+#OPENOCD_ZEPHYR_VERSION = af169e805396e5f7ab926aa32ba293a618a894f7
+
 OPENOCD_ZEPHYR_SITE = git://github.com/zephyrproject-rtos/openocd.git
 OPENOCD_ZEPHYR_GIT_SUBMODULES = YES
 
