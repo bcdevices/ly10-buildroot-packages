65381240591fd3a1065faf1e10be04a3f1328161
Ivo Clarysse <ivo@bcdevices.com>
2023-10-02 17:22:39 +0200

    TGT-116: avrdude: bcdevices -> v7.2 (#34)
    update avrdude_ly to avrdude v7.2
    
    ---------
    
    Co-authored-by: Eric Rauer <eric@rauer.dev>

--- avrdude_ly/avrdude_ly.mk
+++ avrdude_ly/avrdude_ly.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-AVRDUDE_LY_VERSION = 9cb8d9b
-AVRDUDE_LY_SITE = git://github.com/bcdevices/avrdude
+AVRDUDE_LY_VERSION = v7.2
+AVRDUDE_LY_SITE = git://github.com/avrdudes/avrdude.git
 AVRDUDE_LY_LICENSE = GPL-2.0+
 AVRDUDE_LY_LICENSE_FILES = COPYING
 # Sources coming from git, without generated configure and Makefile.in
